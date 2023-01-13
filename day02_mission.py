ad358fb9bf1d0a2876395c3848ac370cb47f0802
kwon <12182193@inha.edu>
2023-01-13 16:25:43 +0900

    day2_mission_2

--- day02_mission.py
+++ day02_mission.py
@@ -1,11 +1,17 @@
 import random
 
-guess = random.randint(1, 10)   #1에서 10사이의 정수가 임의로 발생
-secret = random.randint(1, 10)
-diff = secret - guess
-if diff > 0:
-    print('too high')
-elif diff == 0:
-    print('just right')
+green = random.randint(0, 1)
+small = random.randint(0, 1)
+
+
+if green:
+    if small:
+        print("pea")
+    else:
+        print("watermelon")
+
 else:
-    print('too low')
+    if small:
+        print("cherry")
+    else:
+        print("pumpkin")
