b941f0f665289d1ee9c7247664c477d024494774
kwon <12182193@inha.edu>
2023-01-16 15:35:34 +0900

    prime numb final ver

--- day3-2.py
+++ day3-2.py
@@ -1,41 +1,21 @@
-#prime number for
-
 # prime number
 
-number = int(input("input number : "))
-is_prime = True
 
+start = int(input("input start number: "))
+end = int(input("input end number: "))
 
-for k in range(2, number):
-    if number % k == 0:
-        is_prime = False
-        break
-    print(k)
+if end < start:
+    start, end = end, start
 
-if is_prime:   #0이면 소수라는 뜻
-    print(f'{number}is prime number!')
+for i in range(start, end+1):
+    #is_prime = True
 
-else:
-    print(f'{number}is not prime number.')
+    if i <= 1:
+        continue
+    for k in range(2, i):
+        if i % k == 0:
+           # is_prime = False
+            break
 
-
-
-
-
-# # prime number while
-#
-# number = int(input("input number : "))
-#
-# counts = 0
-#
-# k = 1
-# while k <= number:
-#     if number % k == 0:
-#         counts = counts + 1
-#     k = k + 1
-#
-# if counts == 2:
-#     print(f'{number}is prime number!')
-#
-# else:
-#     print(f'{number}is not prime number.')
+    else:
+        print(i, end=' ')
