a8cdae4eedd49eef23b4e1a728b8634d2c279f43
kwon <12182193@inha.edu>
2023-01-16 17:42:44 +0900

    assignment 6-2

--- day3-2.py
+++ day3-2.py
@@ -1,21 +1,16 @@
-# 문제 5-6
+# 6-2 문제
 
-# an English submarine 이름은 (Boaty McBoatface)로 지어졌다
-# an Australian racehorse 이름은 (Horsey McHorseface)로 지어졌다
-# a Swedish train 이름은 (Trainy McTrainface)로 지어졌다
+guess_me = 7
+number = 1
 
-# English = 'Boat'
-# Australian = 'Horse'
-# Swedish = 'Train'
-#
-# letter = "an English submarine 이름은 (%s y Mc %s face)로 지어졌다\nan Australian racehorse 이름은 (%s y Mc %s face)로 지어졌다\na Swedish train 이름은 (%s y Mcface)로 지어졌다"
-#
-# print(letter)#
+while True:
+    if number < guess_me:
+        print("too low")
+    elif number == guess_me:
+        print("found it")
+        break
+    else:
+        print("oops")
+        break
+    number += 1
 
-Boaty = 'Boat'
-Horsey = 'Horse'
-Trainy = 'Train'
-
-print(f'Boaty Mc {Boaty}s face',
-f'Horsey Mc {Horsey} face',
-f'Trainy Mc {Trainy} face')
