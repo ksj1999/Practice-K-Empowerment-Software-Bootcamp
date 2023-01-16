7b3f390fea243a726ee5f7933c32f0e6a2989ad1
kwon <12182193@inha.edu>
2023-01-16 13:42:03 +0900

    day3 break

--- day3-2.py
+++ day3-2.py
@@ -1,25 +1,13 @@
-print("My kitty cat likes %s", \n"My kitty cat likes %s", \n"My kitty cat likes %s")
+# while
 
+while True:
+    dan = int(input(('Dan :')))
 
-
-
-
-
-song = """when an eel grabs your arm,
-And it causes great harm,
-That's - a moray"""
-
-song_list = song.split()
-print(song_list)
-song_list[14] = song_list[14].title()
-song_string = ' '.join(song_list)
-print(song_list)
-
-
-#print(song.replace(' m', ' M'))
-
-idx = song.rfind('m')
-#songnew = song.replace(song[idx], song[idx].upper())
-
-print(song.endswith('moray!'))
-#print(idx, song[idx].upper())
+    if dan >= 2 and dan <=1 9:
+        i = 1
+        while i < 10:
+            print('{0} * {1} = {2}'.format(dan, i, dan * i))
+            i = i + 1
+        break
+    else:
+        print('2에서 9사이의 값을 입력 하세요')
