ebf2c3ce44c31fa604bee8612ee6256eea8de700
kwon <12182193@inha.edu>
2023-01-16 13:14:52 +0900

    day3 after

--- day3-2.py
+++ day3-2.py
@@ -2,6 +2,17 @@ song = """when an eel grabs your arm,
 And it causes great harm,
 That's - a moray"""
 
+song_list = song.split()
+print(song_list)
+song_list[14] = song_list[14].title()
+song_string = ' '.join(song_list)
+print(song_list)
 
-idx = song.find('m')
-print(idx, song[idx].upper())
+
+#print(song.replace(' m', ' M'))
+
+idx = song.rfind('m')
+#songnew = song.replace(song[idx], song[idx].upper())
+
+print(song.endswith('moray!'))
+#print(idx, song[idx].upper())
