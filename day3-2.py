8e909069aabd13eafcd6f71d92f16b86665df60b
kwon <12182193@inha.edu>
2023-01-16 14:34:57 +0900

    prime num ver3.0

--- day3-2.py
+++ day3-2.py
@@ -7,15 +7,15 @@ number = int(input("input number : "))
 counts = 0
 
 k = 1
-for k in range(1, number+1):
+for k in range(2, number):
     if number % k == 0:
         counts = counts + 1
 
-if counts == 2:
-    print(f'{number}is prime number!')
+if counts:   #0이면 소수라는 뜻
+    print(f'{number}is not prime number.')
 
 else:
-    print(f'{number}is not prime number.')
+    print(f'{number}is prime number!')
 
 
 
