1651edc826c4b9fd6fa87bedc374754d0293313d
kwon <12182193@inha.edu>
2023-01-16 17:19:00 +0900

    assignment 5-6

--- day3-2.py
+++ day3-2.py
@@ -1,37 +1,21 @@
-# 연습문제 5-4
-salutation = 'mr'
-name = 'kwon'
-product = 'gun'
-verbed = 'exploded'
-room = 'chamber'
-animals = 'birds'
-amount = 'quantity'
-percent = '30'
-spokesman  = 'joe'
-job_title = 'CEO'
+# 문제 5-6
 
+# an English submarine 이름은 (Boaty McBoatface)로 지어졌다
+# an Australian racehorse 이름은 (Horsey McHorseface)로 지어졌다
+# a Swedish train 이름은 (Trainy McTrainface)로 지어졌다
 
-
-letter = '\f\fDear {0} {1},\n\n\f\fThank you for your letter. ' \
-         'We are sorry that our {2} {3} in your {4}. ' \
-         'Please note that it should never be used in a {4}, ' \
-         'especially near any {5}\n\n\f\fSend us your receipt and {6} for shipping and handling. ' \
-         'We will send you another {2} that, in our tests, is {7}% less likely to have {3}\n\n\f\f' \
-         'Thank you for your support.' \
-         '\n\f\fSincerly,\n\f\f{8}\n\f\f{9}'.format(salutation, name, product, verbed, room, animals, amount, percent, spokesman, job_title)
-
-print(letter)
-
-
-# letter =         Dear {salutation} {name},
+# English = 'Boat'
+# Australian = 'Horse'
+# Swedish = 'Train'
 #
-#         Thank you for your letter. We are sorry that our {product} {verbed} in your
-# {room}. Please note that it should never be used in a {room}, especially near any {animals}
+# letter = "an English submarine 이름은 (%s y Mc %s face)로 지어졌다\nan Australian racehorse 이름은 (%s y Mc %s face)로 지어졌다\na Swedish train 이름은 (%s y Mcface)로 지어졌다"
 #
-#         Send us your receipt and {amount} for shipping and handling.
-# We will send you another {product} that, in our tests, is {percent}% less likely to have {verbed}
-#
-#         Thank you for your support.
-#         Sincerly,
-#         {spokesman}
-#         {job_title}
+# print(letter)#
+
+Boaty = 'Boat'
+Horsey = 'Horse'
+Trainy = 'Train'
+
+print("Boaty Mc %s face" % Boaty,
+"Horsey Mc %s face" % Horsey,
+"Trainy Mc %s face" % Trainy)
