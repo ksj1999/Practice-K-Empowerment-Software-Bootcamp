adb5873ec76335101e725ca1c3174b7495489f46
kwon <12182193@inha.edu>
2023-01-16 14:17:01 +0900

    day3 more

--- day3-2.py
+++ day3-2.py
@@ -3,11 +3,10 @@
 while True:
     dan = int(input('Dan (0 to quit):'))
 
-    if dan == 0: exit()
-        i = 1
-        while i < 10:
-            print('{0} * {1} = {2}'.format(dan, i, dan * i))
-            i = i + 1
+    if dan == 0: #exit()
         break
+    if 1 < dan < 10:
+        for i in range(1, 10):
+            print('{0} * {1} = {2}'.format(dan, i, dan * i))
     else:
         print('2에서 9사이의 값을 입력 하세요')
