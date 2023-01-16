e37df3f9f07bfdfe91865fad7df2a20eabbc9118
kwon <12182193@inha.edu>
2023-01-16 16:48:08 +0900

    assignment 5-4

--- day3-2.py
+++ day3-2.py
@@ -1,21 +1,37 @@
-# prime number
+# 연습문제 5-4
+salutation = 'mr'
+name = 'kwon'
+product = 'gun'
+verbed = 'exploded'
+room = 'chamber'
+animals = 'birds'
+amount = 'quantity'
+percent = '30'
+spokesman  = 'joe'
+job_title = 'CEO'
 
 
-start = int(input("input start number: "))
-end = int(input("input end number: "))
 
-if end < start:
-    start, end = end, start
+letter = '\f\fDear {0} {1},\n\n\f\fThank you for your letter. ' \
+         'We are sorry that our {2} {3} in your {4}. ' \
+         'Please note that it should never be used in a {4}, ' \
+         'especially near any {5}\n\n\f\fSend us your receipt and {6} for shipping and handling. ' \
+         'We will send you another {2} that, in our tests, is {7}% less likely to have {3}\n\n\f\f' \
+         'Thank you for your support.' \
+         '\n\f\fSincerly,\n\f\f{8}\n\f\f{9}'.format(salutation, name, product, verbed, room, animals, amount, percent, spokesman, job_title)
 
-for i in range(start, end+1):
-    #is_prime = True
+print(letter)
 
-    if i <= 1:
-        continue
-    for k in range(2, i):
-        if i % k == 0:
-           # is_prime = False
-            break
 
-    else:
-        print(i, end=' ')
+# letter =         Dear {salutation} {name},
+#
+#         Thank you for your letter. We are sorry that our {product} {verbed} in your
+# {room}. Please note that it should never be used in a {room}, especially near any {animals}
+#
+#         Send us your receipt and {amount} for shipping and handling.
+# We will send you another {product} that, in our tests, is {percent}% less likely to have {verbed}
+#
+#         Thank you for your support.
+#         Sincerly,
+#         {spokesman}
+#         {job_title}
