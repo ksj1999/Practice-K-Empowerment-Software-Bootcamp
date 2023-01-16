6e1a4fc39df5d65fb62f2f4ba9b217445261ccb2
kwon <12182193@inha.edu>
2023-01-16 13:26:03 +0900

    day 3 exercise

--- day3-2.py
+++ day3-2.py
@@ -1,3 +1,10 @@
+print("My kitty cat likes %s", \n"My kitty cat likes %s", \n"My kitty cat likes %s")
+
+
+
+
+
+
 song = """when an eel grabs your arm,
 And it causes great harm,
 That's - a moray"""
