8ca9b34a96d3300e6dc9f919f54a04c3250e33a6
kwon <12182193@inha.edu>
2023-01-17 13:41:26 +0900

    generator

--- day4.py
+++ day4.py
@@ -1,62 +1,14 @@
-# list
-import copy
-
-a = [1, 2, [5, -9]]
-b = copy.deepcopy(a)
-c = list(a)
-d = a[:]
-a[1] = -77  #immutable
-a[2][1] = 7  # mutable, deep copy / B not affected
-print(a, b)
+#list comprehension
+#
+# odd_lists = []
+# for i in range(1, 10):
+#     if i % 2 == 1:
+#         odd_lists.append(i)
+# print(odd_lists)
 
 
+odd_lists = [i for i in range(1,  11) if i % 2 == 1]
+print(odd_lists, type(odd_lists))
 
-# a = [1, 2, [5, -9]]
-# b = copy.deepcopy(a)
-# c = list(a)
-# d = a[:]
-# a[2][1] = 7  # mutable, deep copy / B not affected
-# print(a, b)
-
-
-
-
-# a = [1, 2, 3]
-# b = a.copy()
-# c = list(a)
-# d = a[:]
-# a[2] = 'sw'  #immutable, b/c/d/ not affected
-# print(a, b, c, d)
-
-
-primes = [2, 19, 3, 5, 7, 11]
-primes_cp = primes
-print(primes)
-print(primes_cp)
-primes[-1] = 'lunch time'
-print(primes)
-print(primes_cp)
-primes[0] = 'morning coffee'
-print(primes)
-print(primes_cp)
-
-
-
-# #TypeError: '<' not supported between instances of 'str' and 'int'
-# #mixed = [6, 4, 5, 'A', 7, '트와이스', 'B', 'b', '마마무']
-# mixed = ['6', '4', '5', 'A', '7', '트와이스', 'B', 'b', '마마무']
-# # mixed.sort()
-# mixed.sort(reverse=True)
-# print(mixed)
-
-
-
-# primes = [2, 19, 3.0, 5, 7, 11]
-# print(primes)
-# primes.sort()
-# print(primes)
-
-
-# primes_sorted = sorted(primes)
-# print(primes)
-# print(primes_sorted)
+odd_tuples = (i for i in range(1,  11) if i % 2 == 1)
+print(odd_tuples, type(odd_tuples))
