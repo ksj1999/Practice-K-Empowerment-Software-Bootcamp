85be87909e0181e06c8f3704d4d71119b414fe8f
kwon <12182193@inha.edu>
2023-01-17 14:43:42 +0900

    food menu ver2.0

--- day4.py
+++ day4.py
@@ -1,7 +1,7 @@
 alcohol_foods = {
     '맥주' : '치킨',
     '소주' : '골뱅이소면',
-    '와인' : '치즈',
+    '위스키' : '치즈',
     '고량주' : '짬뽕'
 }
 alcohol_list = list(alcohol_foods) # extract keys
@@ -11,16 +11,16 @@ while True:
         print('다음에 또 오세요~')
         break
     elif alcohol == '1':
-        print(f'맥주에 어울리는 안주는 {alcohol_foods["맥주"]}입니다')
+        print(f'{alcohol_list[0]}에 어울리는 안주는 {alcohol_foods[alcohol_list[0]]}입니다')
         pass
     elif alcohol == '2':
-        print(f'소주에 어울리는 안주는 {alcohol_foods["소주"]}입니다')
+        print(f'{alcohol_list[1]}에 어울리는 안주는 {alcohol_foods[alcohol_list[1]]}입니다')
         pass
     elif alcohol == '3':
-        print(f'와인에 어울리는 안주는 {alcohol_foods["와인"]}입니다')
+        print(f'{alcohol_list[2]}에 어울리는 안주는 {alcohol_foods[alcohol_list[2]]}입니다')
         pass
     elif alcohol == '4':
-        print(f'고량주에 어울리는 안주는 {alcohol_foods["고량주"]}입니다')
+        print(f'{alcohol_list[3]}에 어울리는 안주는 {alcohol_foods[alcohol_list[3]]}입니다')
         pass
     else:
         print('메뉴에서 골라주세요.')
