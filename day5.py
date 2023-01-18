ef979158f4ae34f35c63480352ad1e08c83cbbc9
kwon <12182193@inha.edu>
2023-01-18 16:11:00 +0900

    9-1

--- day5.py
+++ day5.py
@@ -1,27 +1,6 @@
-# lambda
-import random
-#
-# def process(no_lists, f):
-#     for no in no_lists:
-#         print(f(no))
-#
-#
-# def squares(n):
-#     """
-#     제곱 함수
-#     :param n: integer
-#     :return: integer
-#     """
-#
-#     return n * n
+# 9-1
 
+def good():
+    return ['Harry', 'Ron', 'Hermione']
 
-def process(no_lists, f):
-    for no in no_lists:
-        print(f(no))
-
-
-
-numbers = [random.randint(1, 100) for i in range(5)]
-print(numbers)
-process(numbers, lambda x: x * x)
+print(good(), type(good()))
