3eb6f6b4ff12ef531d64f93d07422cfff298097f
kwon <12182193@inha.edu>
2023-01-18 16:57:31 +0900

    9-2

--- day5.py
+++ day5.py
@@ -1,6 +1,14 @@
-# 9-1
+# 9-2
 
-def good():
-    return ['Harry', 'Ron', 'Hermione']
+def get_odds(first=0, last=10, step=1):
+    number = first
+    while number < last:
+        if number % 2 == 1:
+            yield number
+        number += step
+    else:
+        number += step
 
-print(good(), type(good()))
+ranger = get_odds(1, 11)
+for x in ranger:
+    print(x)
