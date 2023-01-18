79dd434ce0b39422a1c63eda8389a27a25492aff
kwon <12182193@inha.edu>
2023-01-18 15:34:30 +0900

    closures

--- day5.py
+++ day5.py
@@ -1,38 +1,18 @@
-# function
+# closures
 
-def inha():
-    """
-    숫자 출력
-    :return:
-    """
-    print(60)
+def calculate():
+    x = 1
+    y = 2
+    temp = 0
+    def add_sub(n):
+        nonlocal temp
+       #  x = 11  # local variable
+        temp = temp + x + n - y
+        return temp
+    print('once')
+    return add_sub
 
+c1 = calculate()
+for i in range(5):
+    print(c1(i))
 
-def call_func(f):
-    """
-    매개변수로 함수를 넘겨받아 실행
-    :param f: 매개변수가 함수
-    :return:
-    """
-    f()  #넘겨받은 함수 실행
-
-
-def subtract(n1, n2):
-    print(n1, n2)
-
-def run_func(f, arg1, arg2):
-
-a = (5, 7, -11)
-print(sum(a))
-    """
-    
-    :param f: 
-    :param arg1: 
-    :param arg2: 
-    :return: 
-    """
-    f(arg1, arg2)
-run_func(subtract, 99, 88)
-
-call_func(inha)
-print(type(call_func))
