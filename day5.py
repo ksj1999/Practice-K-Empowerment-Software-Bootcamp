0d66f775fd35841f99be27191ee479865e524547
kwon <12182193@inha.edu>
2023-01-18 13:33:38 +0900

    fee calculator

--- day5.py
+++ day5.py
@@ -1,21 +1,40 @@
 # function
+import random
 
-def calucate_fee(*args):
+# def calucate_fee(*args):...
+def calucate_fee(args) -> list:
+
     """
     놀이공원 요금 계산
-    :param args: ages
-    :return: 지불할 총 입장료
+    :param args: ages in list
+    :return: 전체 인원 수, 어름 수, 아이 수, 지불할 총 입장료
     """
     total = 0
+    adults = 0
+    kids = 0
     for age in args:
         if 19 <= age:  # adult
             total = total + 10000
+            adults= adults + 1
+
         else:
             total = total + 3000
-    return total
+            kids = kids + 1
+    return [len(args), adults, kids, total]
 
-print(calucate_fee(20, 20, 25))
-print(calucate_fee(45, 43, 10, 7))
+
+
+no_of_visitor = int(input('몇 분 이세요? '))
+ages = [random.randint(1, 60) for age in range(no_of_visitor)]
+results = calucate_fee(ages)
+print(f'{results[0]}명, 방문하셨고 어른{results[1]}명, 아이 {results[2]}명, 총 요금은 {results[-1]}입니다')
+
+
+
+# 5분 방문하셨고 어른 2명 아이 3명 총 요금은 10000원 입니다
+#
+# print(calucate_fee(20, 20, 25))
+# print(calucate_fee(45, 43, 10, 7))
 
 #
 # def do_nothing():
