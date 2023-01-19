08c23bba3d9a597700acb88eb30868b6f1b0cea9
kwon <12182193@inha.edu>
2023-01-19 16:13:04 +0900

    9-4

--- day6.py
+++ day6.py
@@ -1,28 +1,8 @@
-# class
-
-class Pokemon:
-    def __init__(self, name, owner, skills):
-        self.name = name
-        self.owner = owner
-        self.skills = skills.split('/')
-
-        print(f"포켓몬 {name} 생산됨")
-
-    def info(self):
-        print(f"{self.owner}의 포켓몬은 {self.name}입니다")
-        for skill in self.skills:
-            print(skill)
+try:
+    expr = input('분자 분모 입력 (띄어쓰기로 구분) : ').split()
+    print(int(expr[0]) / int(expr[1]))
 
 
-class Pikachu(Pokemon):  #inheritance
-    pass
-
-
-pi1 = Pikachu('피카츄', '텐트', '번개')
-pi1.info()
-
-
-p1 = Pokemon('피카츄', '한지우', '50만 볼트/100만 볼트/번개')
-p2 = Pokemon('꼬부기', '오바람', '고속스핀/거품/몸통박치기/하이드로펌프')
-
+except Exception:
+    print(f'Caught an oops')
 
