cf15bcd6e52d6fa8f5ef498b0f0b7c373c29b2f6
kwon <12182193@inha.edu>
2023-01-19 13:41:48 +0900

    exception

--- day6.py
+++ day6.py
@@ -1,27 +1,44 @@
-def factorial_iter(n):
-    """
-    반복문을 사용한 펙토리얼 함수
-    :param n: n!
-    :return: 팩토리얼 계산 결과 값
-    """
-    result = 1
-    for k in range(1, n+1):
-        result = result * k
-    return result
+try:
+    expr = input('분자 분모 입력 (띄어쓰기로 구분) : ').split()
+    print(int(expr[0]) / int(expr[1]))
 
-def factorial_recu(n):
-    """
-    재귀함수를 사용한 팩토리얼 게산 함수
-    :param n: n!
-    :return: 자기 자신을 호출 또는 1
-    """
-    if n == 1:  #끝나는 조건
-        return 1
-    else:
-        return factorial_recu(n-1) * n
+except ValueError as err:
+    print(f'숫자를 입력해주세요({err})')
 
+except ZeroDivisionError as err:
+    print(f'분모에 0이 올 수 없습니다({err})')
 
+except IndexError as err:
+    print(f'입력 값의 범위에 문제가 있습니다({err})')
+
+except Exception as other:
+    print(f'에러발생: {other}')
+
+else:  #예외가 발생하지 않았을 때
+    print('프로그램 정상', end=' ')
+
+finally:  #예외 발생 여부와 상관 없이 무조건 실행
+    print('종료')
 
 
-print(factorial_iter(10))
-print(factorial_recu(10))
+
+
+#value error
+#zerodivision error
+
+
+
+# def div_calc(a, b):
+#     """
+#     나누기 함수
+#     :param a: 분자
+#     :param b: 분모
+#     :return: 계산결과
+#     """
+#
+#     return a / b
+#
+#
+#
+# print(div_calc(15, 3))
+# print(div_calc(7, 0))
