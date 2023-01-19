9d5eff406f978dd721a78037d69b36bd414c2029
kwon <12182193@inha.edu>
2023-01-19 11:34:14 +0900

    recu

--- day6.py
+++ day6.py
@@ -1,18 +1,26 @@
-g = 1  # global
+def factorial_iter(n):
+    """
+    반복문을 사용한 펙토리얼 함수
+    :param n: n!
+    :return: 팩토리얼 계산 결과 값
+    """
+    result = 1
+    for k in range(1, n+1):
+        result = result * k
+    return result
 
-def print_global():
-    # g = 1  # local
-    print(g)
+def factorial_recu(n):
+    """
+    재귀함수를 사용한 팩토리얼 게산 함수
+    :param n: n!
+    :return: 자기 자신을 호출 또는 1
+    """
+    if n == 1:  #끝나는 조건
+        return 1
+    else:
+        return factorial_recu(n-1) * n
 
-def change_print_global():
-    global g
-    print(g)
-    g = 2
-    print(g)
 
 
-change_print_global()
-print_global()
-print(g)
-print(globals())
-print(__name__)
+print(factorial_recu(5))
+print(factorial_iter(5))
