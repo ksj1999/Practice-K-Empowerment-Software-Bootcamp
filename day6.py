9975b9c67440af23cdd8932a06d66320adf31385
kwon <12182193@inha.edu>
2023-01-19 15:30:59 +0900

    pokemon

--- day6.py
+++ day6.py
@@ -1,7 +1,36 @@
-groups = {'빅뱅' : ['GD', '태양', '탑', '대성', '승리'], '마마무' : ['문별', '솔라', '휘인', '화사']}
+# class
 
-for group, members in groups.items():
-    print(f'{group}의 멤버')
-    for member in members:
-        if member != "승리":
-            print(member)
+class Pokemon:
+    def __init__(self, name, owner, skills):
+        self.name = name
+        self.owner = owner
+        self.skills = skills.split('/')
+
+        print(f"포켓몬 {name} 생산됨")
+
+    def info(self):
+        print(f"{self.owner}의 포켓몬은 {self.name}입니다")
+        for skill in self.skills:
+            print(skill)
+
+
+p1 = Pokemon('피카츄', '한지우', '50만 볼트/100만 볼트/번개')
+p2 = Pokemon('꼬부기', '오바람', '고속스핀/거품/몸통박치기/하이드로펌프')
+
+
+p2.info()
+p1.info()
+# print(p2, skills)
+# print(p1, p2)
+
+
+
+
+# class Pokemon:
+#     def __init__(self):  #객체 생성 시 동작
+#         print("포켓몬 객체 생선됨")
+#
+#
+# p1 = Pokemon()
+# p2 = Pokemon()
+# print(p1, p2)
