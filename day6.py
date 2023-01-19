1be9ef176ed0e42d59b12940f5865b1a82c27c06
kwon <12182193@inha.edu>
2023-01-19 14:20:02 +0900

    9-2

--- day6.py
+++ day6.py
@@ -1,6 +1,14 @@
-# ex 9-1
+# ex 9-2
 
-def good():
-    return ['harry', 'ron', 'hermione']
+cnts = 0
 
-print(good())
+def get_odds():
+    for num in range(10):
+        if num % 2:
+            yield num
+
+odds = get_odds()
+for i in odds:
+    cnts += 1
+    if cnts ==3:
+        print(i)
