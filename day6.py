55ba8ab22dec1e604d3fbc8a5c02da321ef6cd65
kwon <12182193@inha.edu>
2023-01-19 14:46:32 +0900

    practice

--- day6.py
+++ day6.py
@@ -1,14 +1,7 @@
-# ex 9-2
+groups = {'빅뱅' : ['GD', '태양', '탑', '대성', '승리'], '마마무' : ['문별', '솔라', '휘인', '화사']}
 
-cnts = 0
-
-def get_odds():
-    for num in range(10):
-        if num % 2:
-            yield num
-
-odds = get_odds()
-for i in odds:
-    cnts += 1
-    if cnts ==3:
-        print(i)
+for group, members in groups.items():
+    print(f'{group}의 멤버')
+    for member in members:
+        if member != "승리":
+            print(member)
