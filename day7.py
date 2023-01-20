488390a4081517f8e7d0b47ca2b3c537d17656ab
kwon <12182193@inha.edu>
2023-01-20 15:38:20 +0900

    rect add

--- day7.py
+++ day7.py
@@ -36,15 +36,26 @@ class Rectangle(shape):
     def get_area(self):
         return self.width * self.length
 
+    def __repr__(self):
+        return f'사각형의 좌표는 x:{r1.x}, y:{r1.y}이고 넓이는 {r1.get_area()}입니다'
+
+    def __add__(self, other):
+        # 두 사각형 넓이의 합
+        # return (self.width * self.length) + (other.width * other.length)
+        # 각 사각형의 width의 합과 length의 합의 곱
+        return Rectangle(0, 0, (self.width+other.width), (self.length+other.length))
+
 
 cy1 = Cyilinder(20, 20, 10.0, 2)
 c1 = Circle(100, 100, 10.0)
 c2 = Circle(50, 50, 2.0)
 r1 = Rectangle(100, 50, 5, 2)
+r2 = Rectangle(70, 30, 10, 15)
 
-
+print(r2)
 print(cy1.get_area())
-print(f'사각형의 좌표는 x:{r1.x}, y:{r1.y}이고 넓이는 {r1.get_area()}입니다')
+print(r1)
+print(r1 + r2)
 print(r1.get_area())
 print(c1.get_area())
 print(c2.get_area())
