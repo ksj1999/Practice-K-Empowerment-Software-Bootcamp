f8b0abbe0da1e76899502ad3e14d93c6cea5ec80
kwon <12182193@inha.edu>
2023-02-02 15:39:25 +0900

    day14 pokemon_2

--- day7.py
+++ day7.py
@@ -1,22 +1,27 @@
-# pokemon game v0.3
-# 중복코드 제거, getter setter -> property
+# pokemon game v0.4
+# getter setter -> property -> decorator
+# __hiddenfield
 
 class Pokemon:
     def __init__(self, owner, skills):
-        self.hidden_owner = owner
+        self.__hidden_owner = owner  # like private
         self.skills = skills.split('/')
         print(f"포켓몬 생성 :", end=' ')
-    def get_owner(self):
-        return self.hidden_owner
-    def set_owner(self, owner):
-        self.hidden_owner = owner
+
+    @property
+    def owner(self):
+        return self.__hidden_owner
+
+    @owner.setter
+    def owner(self, owner):
+        self.__hidden_owner = owner
 
     def info(self):
         print(f"{self.owner}의 포켓몬이 사용 가능한 스킬")
         for i in range(len(self.skills)):
             print(f'{i+1} : {self.skills[i]}')
 
-    owner = property(get_owner, set_owner)
+    #owner = property(get_owner, set_owner)
         # for skill in self.skills:
         #     print(f'{skill}')
 
@@ -27,20 +32,15 @@ class Pikachu(Pokemon):  # inheritance
         super().__init__(owner, skills)
         self.name = "피카츄"
         print(f"{self.name}")
-
     def attack(self, idx):  # override
         print(f'{self.owner}의 {self.name}가 {self.skills[idx]} 공격(전기) 시전!')
-
-
 class Ggoboogi(Pokemon):  # inheritance
     def __init__(self, owner, skills):
         super().__init__(owner, skills)
         self.name = "꼬부기"
         print(f"{self.name}")
-
     def attack(self, idx):  # override
         print(f'{self.owner}의 {self.name}가 {self.skills[idx]} 공격(물) 시전!')
-
     def swim(self):
         print(f'{self.name}가 수영을 합니다')
 class Pairi(Pokemon):  # inheritance
@@ -48,11 +48,8 @@ class Pairi(Pokemon):  # inheritance
         super().__init__(owner, skills)
         self.name = "파이리"
         print(f"{self.name}")
-
     def attack(self, idx):  # override
         print(f'{self.owner}의 {self.name}가 {self.skills[idx]} 공격(불) 시전!')
-
-
 while True:
     menu = input('1) 포켓몬 생성  2) 프로그램 종료 : ')
     if menu == '2':
@@ -64,6 +61,8 @@ while True:
         s = input('사용 가능한 기술 입력(/로 구분하여 입력) :')
         if pokemon == '1':
             p = Pikachu(n, s)
+            # p.owner = "한지우"  # setter
+            # p.__hidden_owner = "한지우"  # 허가되지 않은 접근
         elif pokemon == '2':
             p = Ggoboogi(n, s)
         elif pokemon == '3':
