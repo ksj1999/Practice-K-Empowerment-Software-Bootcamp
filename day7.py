9d89a130b6990322a81bf6a41768a65f9b606750
kwon <12182193@inha.edu>
2023-02-02 15:13:47 +0900

    day14 pokemon-1

--- day7.py
+++ day7.py
@@ -1,30 +1,27 @@
-# pokemon game v0.2
-# 중복코드 제거, getter setter
+# pokemon game v0.3
+# 중복코드 제거, getter setter -> property
 
 class Pokemon:
     def __init__(self, owner, skills):
         self.hidden_owner = owner
         self.skills = skills.split('/')
         print(f"포켓몬 생성 :", end=' ')
-
     def get_owner(self):
         return self.hidden_owner
-
     def set_owner(self, owner):
         self.hidden_owner = owner
 
     def info(self):
-        print(f"{self.get_owner()}의 포켓몬이 사용 가능한 스킬")
+        print(f"{self.owner}의 포켓몬이 사용 가능한 스킬")
         for i in range(len(self.skills)):
             print(f'{i+1} : {self.skills[i]}')
 
+    owner = property(get_owner, set_owner)
         # for skill in self.skills:
         #     print(f'{skill}')
 
     def attack(self, idx):
         print(f'{self.skills[idx]} 공격 시전!')
-
-
 class Pikachu(Pokemon):  # inheritance
     def __init__(self, owner, skills):
         super().__init__(owner, skills)
@@ -32,7 +29,7 @@ class Pikachu(Pokemon):  # inheritance
         print(f"{self.name}")
 
     def attack(self, idx):  # override
-        print(f'{self.get_owner()}의 {self.name}가 {self.skills[idx]} 공격(전기) 시전!')
+        print(f'{self.owner}의 {self.name}가 {self.skills[idx]} 공격(전기) 시전!')
 
 
 class Ggoboogi(Pokemon):  # inheritance
@@ -42,12 +39,10 @@ class Ggoboogi(Pokemon):  # inheritance
         print(f"{self.name}")
 
     def attack(self, idx):  # override
-        print(f'{self.get_owner()}의 {self.name}가 {self.skills[idx]} 공격(물) 시전!')
+        print(f'{self.owner}의 {self.name}가 {self.skills[idx]} 공격(물) 시전!')
 
     def swim(self):
         print(f'{self.name}가 수영을 합니다')
-
-
 class Pairi(Pokemon):  # inheritance
     def __init__(self, owner, skills):
         super().__init__(owner, skills)
@@ -55,7 +50,7 @@ class Pairi(Pokemon):  # inheritance
         print(f"{self.name}")
 
     def attack(self, idx):  # override
-        print(f'{self.get_owner()}의 {self.name}가 {self.skills[idx]} 공격(불) 시전!')
+        print(f'{self.owner}의 {self.name}가 {self.skills[idx]} 공격(불) 시전!')
 
 
 while True:
