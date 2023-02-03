6ec861e90c9a31a2f9f4fe4914c1bf0c39c118e3
kwon <12182193@inha.edu>
2023-02-03 11:14:37 +0900

    day15 Linerlist3-2

--- day15.py
+++ day15.py
@@ -1,15 +1,21 @@
-pokemons = []  # 빈 배열
+pokemons = ["피카츄", "라이츄", "파이리", "꼬부기", "이상해"]
 
 
-def add_data(pokemon):
-    pokemons.append(None)
-    #pokemons[len(pokemons)-1] = pokemon
-    pokemons[-1] = pokemon
+def insert_data(index, pokemon):
+    if index < 0 or index > len(pokemons):
+        print("out of range.")
+        return
 
-add_data('피카츄')
-add_data('라이츄')
-add_data('파이리')
-add_data('꼬부기')
-add_data('이상해')
+    pokemons.append(None)  # 빈칸 추가
 
+    for i in range(len(pokemons) - 1, index, -1):
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
+
+    pokemons[index] = pokemon  # 지정한 위치에 친구 추가
+
+
+insert_data(2, '어니부기')
+print(pokemons)
+insert_data(6, '거북왕')
 print(pokemons)
