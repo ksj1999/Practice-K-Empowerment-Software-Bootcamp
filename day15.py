8cf33787bfd8969c43f265ac012ee8625804e79e
kwon <12182193@inha.edu>
2023-02-03 14:25:10 +0900

    day15 3-7

--- day15.py
+++ day15.py
@@ -1,51 +1,46 @@
-def print_poly(px):
+def print_poly(px, tx):
     """
     다항식을 포맷에 맞게 출력하는 함수
     :param px: 계수를 원소로 가지고 있는 list
+    :param tx: 차수를 원소로 가지고 있는 list
     :return: 다항식 문자열
     """
-    term = len(px) - 1
     poly_str = "P(x) = "
 
     for i in range(len(px)):
+        term = tx[i]
         coef = px[i]
-        # if coef >= 0:
-        #     poly_str = poly_str + "+"
-
-        if i > 0 and coef > 0:
+        if coef >= 0:
             poly_str = poly_str + "+"
-        elif coef == 0:
-            term = term - 1
-            continue
 
         poly_str = poly_str + f'{coef}x^{term} '
-        term = term - 1
 
     return poly_str
 
 
-def calc_poly(x_val, p_x):
+def calc_poly(x_val, px, tx):
     """
     다항식의 산술연산을 하는 함수
     :param x_val: x값 integer
-    :param p_x: 계수를 원소로 가지고 있는 list
+    :param px: 계수를 원소로 가지고 있는 list
+    :param tx: 차수를 원소로 가지고 있는 list
     :return: 다항식 계산 결과 값 integer
     """
     return_val = 0
-    term = len(p_x) - 1
 
     for i in range(len(px)):
-        coef = p_x[i]
+        term = tx[i]
+        coef = px[i]
         return_val = return_val + coef * x_val ** term
-        term = term - 1
 
     return return_val
 
 
-px = [3, -4, 0, 6]
+px = [3, -4, 5]
+tx = [300, 20, 0]
 
 if __name__ == "__main__":
-    print(print_poly(px))
+    print(print_poly(px, tx))
 
     x_value = int(input("X 값 : "))
-    print(calc_poly(x_value, px))
+    print(calc_poly(x_value, px, tx))
