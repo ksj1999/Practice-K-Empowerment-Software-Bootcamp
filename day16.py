79c7a840fd9e4bed1a680968f5a6313376a71088
kwon <12182193@inha.edu>
2023-02-06 13:07:37 +0900

    day16-1

--- day16.py
+++ day16.py
@@ -2,28 +2,29 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.link = None
-
-
 def print_nodes(start):
-	@@ -53,30 +10,96 @@ def print_nodes(start):
+    current = start
+    if current == None :
         return
     print(current.data, end=' ')
-    while current.link != None:
+    while current.link != start:  #
         current = current.link
         print(current.data, end=' ')
     print()
-
-
 def insert_nodes(find_data, insert_data):
     global head, current, pre
     if head.data == find_data:
         node = Node(insert_data)
         node.link = head
+        last = head  #
+        while last.link != head:  #
+            last = last.link  #
+        last.link = node  #
         head = node
         return
 
     current = head
-    while current.link != None:
+    while current.link != head:  #
         pre = current
         current = current.link
         if current.data == find_data:
@@ -34,18 +35,17 @@ def insert_nodes(find_data, insert_data):
 
     node = Node(insert_data)
     current.link = node
+    node.link = head  #
 
 
 def delete_nodes(delete_data):
     global head, current, pre
-
     if head.data == delete_data:
         print('첫 번째 노드 삭제 완료')
         current = head
         head = head.link
         del current
         return
-
     current = head
     while current.link != None:
         pre = current
@@ -55,37 +55,29 @@ def delete_nodes(delete_data):
             pre.link = current.link
             del current
             return
-
     print('삭제할 노드를 찾지 못함')
-
-
 def find_nodes(find_data):
     global head, current, pre
-
     current = head
     if current.data == find_data:
         return current
-
     while current.link != None:
         current = current.link
         if current.data == find_data:
             return current
-
     return Node(None)
-
-
 head, current, pre = None, None, None
 data_array = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
-
-
 if __name__ == "__main__":
     node = Node(data_array[0])
     head = node
+    node.link = head  #
 
     for data in data_array[1:]:
         pre = node
         node = Node(data)
         pre.link = node
+        node.link = head  #
 
     print_nodes(head)
     insert_nodes("피카츄", "잠만보")
@@ -94,11 +86,3 @@ if __name__ == "__main__":
     print_nodes(head)
     insert_nodes("성윤모", "거북왕")
     print_nodes(head)
-    delete_nodes("잠만보")
-    print_nodes(head)
-    delete_nodes("어니부기")
-    print_nodes(head)
-    delete_nodes("강찬석")
-    print_nodes(head)
-    print(find_nodes("파이리").data)
-    print(find_nodes("박민석").data)
