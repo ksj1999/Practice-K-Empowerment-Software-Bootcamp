1a42e96d613a79d2bd7fbc99dcfa3cd3c107ad3d
kwon <12182193@inha.edu>
2023-02-06 14:59:09 +0900

    day16 stack-2

--- day16.py
+++ day16.py
@@ -1,7 +1,5 @@
-stack = [None for _ in range(10)]
+stack = [None for _ in range(5)]
 top = -1
-
-
 
 top = top + 1
 stack[top] = '커피'
@@ -12,6 +10,32 @@ stack[top] = '녹차'
 top = top + 1
 stack[top] = '꿀물'
 
+top = top + 1
+stack[top] = '오렌지주스'
 
-for i in range(len(stack)-1, -1, -1):
+top = top + 1
+stack[top] = '바닐라라떼'
+
+
+# stack overflow
+# top = top + 1
+# stack[top] = '카라멜마끼아또'
+
+for i in range(len(stack) -1, -1, -1):
+    print(stack[i])
+
+data = stack[top]
+stack[top] = None
+top = top - 1
+print(data)
+
+for i in range(len(stack) -1, -1, -1):
+    print(stack[i])
+
+data = stack[top]
+stack[top] = None
+top = top - 1
+print(data)
+
+for i in range(len(stack) -1, -1, -1):
     print(stack[i])
