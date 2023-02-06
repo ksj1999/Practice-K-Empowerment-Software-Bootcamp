4bebd5516ff25f54e71918f1215dc0eaa9191aa2
kwon <12182193@inha.edu>
2023-02-06 15:49:40 +0900

    day16 stack-3

--- day16.py
+++ day16.py
@@ -1,41 +1,71 @@
-stack = [None for _ in range(5)]
+def is_stack_full():
+    global SIZE, stack, top
+    if top >= SIZE - 1:
+        return True
+    else:
+        return False
+
+
+def is_stack_empty():
+    global SIZE, stack, top
+    if top == - 1:
+        return True
+    else:
+        return False
+
+
+def push(data):
+    global SIZE, stack, top
+    if is_stack_full():
+        print("Stack is full!")
+        return
+    top = top + 1
+    stack[top] = data
+
+
+def pop():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("Stack is empty~")
+        return
+    temp = stack[top]
+    stack[top] = None
+    top = top - 1
+    return temp
+
+
+def peek():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("Stack is empty~")
+        return None
+    return stack[top]
+
+
+SIZE = int(input("Stack Size : "))
+stack = [None for _ in range(SIZE)]
 top = -1
 
-top = top + 1
-stack[top] = '커피'
+if __name__ == "__main__":
 
-top = top + 1
-stack[top] = '녹차'
+    while True:
+        menu = input("Insert(I)/Extract(E)/Verify(V)/Exit(X) : ")
 
-top = top + 1
-stack[top] = '꿀물'
+        if menu == 'X' or menu == 'x':
+            break
+        elif menu == 'I' or menu == 'i':
+            data = input("Input Data : ")
+            push(data)
+            print("Stack Status : ", stack)
+        elif menu == 'E' or menu == 'e':
+            data = pop()
+            print("Extracted Data : ", data)
+            print("Stack Status : ", stack)
+        elif menu == 'V' or menu == 'v':
+            data = peek()
+            print("Check Data : ", data)
+            print("Stack Status : ", stack)
+        else:
+            print("Input Mismatch")
 
-top = top + 1
-stack[top] = '오렌지주스'
-
-top = top + 1
-stack[top] = '바닐라라떼'
-
-
-# stack overflow
-# top = top + 1
-# stack[top] = '카라멜마끼아또'
-
-for i in range(len(stack)-1, -1, -1):
-    print(stack[i])
-
-data = stack[top]
-stack[top] = None
-top = top - 1
-print(data)
-
-for i in range(len(stack)-1, -1, -1):
-    print(stack[i])
-
-data = stack[top]
-stack[top] = None
-top = top - 1
-print(data)
-
-for i in range(len(stack)-1, -1, -1):
-    print(stack[i])
+    print("Program End")
