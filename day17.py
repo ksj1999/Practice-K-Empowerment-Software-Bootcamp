2a30d05677cd13ed810f316b8090db01e74f3ea0
kwon <12182193@inha.edu>
2023-02-07 09:39:58 +0900

    day17 bracket checker

--- day17.py
+++ day17.py
@@ -42,34 +42,42 @@ def pop():
 def peek():
     global SIZE, stack, top
     if is_stack_empty():
-        print("스택이 비었습니다.")
+        print("Stack is empty")
         return None
     return stack[top]
 
 
+def check_bracket(expr):
+    for ch in expr:
+        if ch in '([{<':
+            push(ch)
+        elif ch in ')]}>':
+            out = pop()
+            if ch == ')' and out == '(':
+                pass
+            elif ch == ']' and out == '[':
+                pass
+            elif ch == '}' and out == '{':
+                pass
+            elif ch == '>' and out == '<':
+                pass
+            else:
+                return False
+        else:
+            pass
+    return is_stack_empty()
+
+
 ## 전역 변수 선언 부분 ##
-SIZE = 5
+SIZE = 100
 stack = [None for _ in range(SIZE)]
 top = -1
 
 ## 메인 코드 부분 ##
 if __name__ == "__main__":
-    urls = ['inha.ac.kr', 'harvard.edu', 'yale.edu']
 
-    for url in urls:
-        push(url)
-        webbrowser.open('http://' + url)
-        print(url, end='-->')
-        time.sleep(1)
+    expression_array = ['2*[3+1)]', ')A+B(', '((A+B)-C', '(A+B]', '(<A+{B-C}/[C*D]>)']
 
-    print("방문 종료")
-    time.sleep(5)
-
-    while True:
-        url = pop()
-        if url == None:
-            break
-        webbrowser.open('http://' + url)
-        print(url, end='-->')
-        time.sleep(1)
-    print("방문 종료")
+    for expression in expression_array:
+        top = -1
+        print(expression, ' : ', check_bracket(expression))
