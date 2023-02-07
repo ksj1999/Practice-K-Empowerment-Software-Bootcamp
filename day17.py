87cec00db23fb856a39bd9cd5af09e2fa1dc224b
kwon <12182193@inha.edu>
2023-02-07 18:30:22 +0900

    day17 assignment

--- day17.py
+++ day17.py
@@ -1,83 +1,69 @@
-import webbrowser
-import time
-
-
-## 함수 선언 부분 ##
-def is_stack_full():
-    global SIZE, stack, top
-    if top >= SIZE - 1:
+def is_queue_full():
+    global SIZE, queue, front, rear
+    if rear == SIZE - 1:
         return True
     else:
         return False
 
 
-def is_stack_empty():
-    global SIZE, stack, top
-    if top == -1:
+def is_queue_empty():
+    global SIZE, queue, front, rear
+    if front == rear:
         return True
     else:
         return False
 
 
-def push(data):
-    global SIZE, stack, top
-    if is_stack_full():
-        print("스택이 꽉 찼습니다.")
+def en_queue(data):
+    global SIZE, queue, front, rear
+    if is_queue_full():
+        print("큐가 꽉 찼습니다.")
         return
-    top += 1
-    stack[top] = data
+    rear += 1
+    queue[rear] = data
 
 
-def pop():
-    global SIZE, stack, top
-    if is_stack_empty():
-        print("스택이 비었습니다.")
+def de_queue():
+    global SIZE, queue, front, rear
+    if is_queue_empty():
+        print("큐가 비었습니다.")
         return None
-    data = stack[top]
-    stack[top] = None
-    top -= 1
+    front += 1
+    data = queue[front]
+    queue[front] = None
+
+    for i in range(front + 1, rear + 1):
+        queue[i - 1] = queue[i]
+        queue[i] = None
+    front = -1
+    rear -= 1
+
     return data
 
 
 def peek():
-    global SIZE, stack, top
-    if is_stack_empty():
-        print("Stack is empty")
+    global SIZE, queue, front, rear
+    if is_queue_empty():
+        print("큐가 비었습니다.")
         return None
-    return stack[top]
+    return queue[front + 1]
 
 
-def check_bracket(expr):
-    for ch in expr:
-        if ch in '([{<':
-            push(ch)
-        elif ch in ')]}>':
-            out = pop()
-            if ch == ')' and out == '(':
-                pass
-            elif ch == ']' and out == '[':
-                pass
-            elif ch == '}' and out == '{':
-                pass
-            elif ch == '>' and out == '<':
-                pass
-            else:
-                return False
-        else:
-            pass
-    return is_stack_empty()
+SIZE = 5
+queue = [None for _ in range(SIZE)]
+front = rear = -1
 
 
-## 전역 변수 선언 부분 ##
-SIZE = 100
-stack = [None for _ in range(SIZE)]
-top = -1
-
-## 메인 코드 부분 ##
 if __name__ == "__main__":
+    en_queue('정국')
+    en_queue('뷔')
+    en_queue('지민')
+    en_queue('진')
+    en_queue('슈가')
+    print("대기 줄 상태 : ", queue)
 
-    expression_array = ['2*[3+1)]', ')A+B(', '((A+B)-C', '(A+B]', '(<A+{B-C}/[C*D]>)']
+    for _ in range(rear + 1):
+        print(de_queue(), '님 식당에 들어감')
+        print("대기 줄 상태 : ", queue)
 
-    for expression in expression_array:
-        top = -1
-        print(expression, ' : ', check_bracket(expression))
+    print("식당 영업 종료!")
